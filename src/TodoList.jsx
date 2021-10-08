f98c617856c28297f4af0b58d0ceea29d10187d7
huynvn <Huy.Nguyen@navigosgroup.com>
2021-10-08 13:39:07 +0700

    Pending todo list

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,13 +1,20 @@
 import React, { useState } from "react";
-import { atom, useRecoilValue } from "recoil";
+import { atom, useRecoilValue, useSetRecoilState } from "recoil";
 
 const todoListState = atom({
   key: "todoListState",
   default: [],
 });
 
+let id = 0;
+function getId() {
+  return id++;
+}
+
 function TodoItemCreator() {
   const [inputValue, setInputValue] = useState("");
+  const setTodoList = useSetRecoilState(todoListState);
+
   const addItem = () => {
     setTodoList((oldTodoList) => [
       ...oldTodoList,
@@ -17,19 +24,25 @@ function TodoItemCreator() {
         isComplete: false,
       },
     ]);
+    setInputValue("");
   };
+
+  const onChange = ({target: {value}}) => {
+    setInputValue(value);
+  }
+
   return (
     <div>
-      <input type="text" style={{ marginLeft: "20px", marginTop: "20px" }} />
-      <button onClick={null}>Add</button>
+      <input type="text" value={inputValue} onChange={onChange} style={{ marginLeft: "20px", marginTop: "20px" }} />
+      <button onClick={addItem}>Add</button>
     </div>
   );
 }
 
-function TodoItem() {
+function TodoItem({item}) {
   return (
     <div>
-      <input type="text" />
+      <input type="text" value={item.text} />
       <input type="checkbox" />
       <button onClick={null}>X</button>
     </div>
@@ -38,16 +51,15 @@ function TodoItem() {
 
 const TodoList = (props) => {
   const todoList = useRecoilValue(todoListState);
+  console.log("🚀 ~ file: TodoList ~ todoList", todoList)
   return (
     <div>
       <TodoItemCreator />
       {todoList.map((todoItem) => (
-        <TodoItem />
+        <TodoItem item={todoItem}/>
       ))}
     </div>
   );
 };
-
-TodoList.propTypes = {};
 
 export default TodoList;
